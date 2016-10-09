a0091d170a2ac97d8a9344c60fc096161a4f3822
Christopher Wells <cwellsny@nycap.rr.com>
2016-10-09 03:16:39 -0400

    Add box generation
    Setup api response boxes so that the are generated on screen as needed.
    
    Closes #8

--- js/importer.js
+++ js/importer.js
@@ -12,15 +12,29 @@ function getUrlVars() {
     return vars;
 }
 
-$.getScript('js/apis.js', function(){ 
-    
-    
-    function importQuery(results, divID)   {
-       var listElements = results.map( resultsToLi );
-       var joinElements = listElements.join("");
-       var htmlElements = "<ul>" + joinElements + "</ul>";
-       $("#" + divID).append( htmlElements );
-    }
+var nextBox = 0;
+var nextRow = 0;
+function generateBox(results, name) {
+    var box = '<div id="box' + nextBox + '" class="col-sm-6">';
+    box = box + '<div class="panel panel-primary">';
+    box = box + '<div class="panel-heading">' + name + '</div>';
+    box = box + '<div id="listBox' + nextBox + '" class="panel-body">';
+    box = box + generateListElements(results) + '</div>';
+    box = box + "</div>";
+    box = box + "</div>";
+
+    $("#results-div").append(box);
+    nextBox++;
+}
+
+function generateListElements(results) {
+    var listElements = results.map( resultsToLi );
+    var joinElements = listElements.join("");
+    var htmlElements = "<ul>" + joinElements + "</ul>";
+    return htmlElements
+}
+
+$.getScript('js/apis.js', function(){
 
     var urlValues = getUrlVars();
     var language = urlValues["language"];
@@ -32,13 +46,10 @@ $.getScript('js/apis.js', function(){
         , "github-issues"
     ];
 
-    console.log(apis);
-    console.log(apis.length);
-
     for (j = 0; j < apis.length; j++) {
         var api = apis[j];
         var id = "listBox" + (j + 1);
         var queryResults = queryAPI(api, language, query);
-        importQuery(queryResults, id);
+        generateBox(queryResults, api);
     }
 });
